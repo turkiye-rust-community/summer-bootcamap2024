652cf1f8f59e461c657bd21f156c4e2ec19eb7a5
Veli Uysal <veliuysaldev@gmail.com>
2024-07-12 23:07:55 +0300

    2. hafta

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::{fmt::format};
-
 fn main() {
     let mut degisken = "Hello Bootcamp2024";
     degisken = "Öğrencileri";
@@ -14,19 +12,19 @@ fn main() {
     //8, 16, 32, 64, 128
     //i8, u32
     //arch -> isize, usize
-    // 32bit laptop, isize -> 
+    // 32bit laptop, isize ->
 
     let int_deg = 12;
-    let int_deg2 : u16 = 12;
+    let int_deg2: u16 = 12;
     println!("{}", int_deg);
     println!("{}", int_deg2);
     let arch_deg: usize = 13;
     let arch_deg2: isize = 13;
     //smart print
 
-    //floating point 
+    //floating point
     //double, float
-    //f32, f64 
+    //f32, f64
 
     let fp_deg = 12.2;
     let fp_deg2: f32 = 12.2;
@@ -34,7 +32,7 @@ fn main() {
     //boolean
     //true, false
     let b_deg = false;
-    let b_deg2 : bool = true;
+    let b_deg2: bool = true;
 
     //character
     //char -> 1 byte, 2 bytes, 4 bytes
@@ -54,26 +52,26 @@ fn main() {
     let nl_deg4 = 0o76571;
     println!("{:x}", nl_deg4);
 
-    //Compound 
+    //Compound
     //Tuple (tip1, tip2, tip3)
     let t_deg = (12, 3.4, 'A');
     println!("{:?}", t_deg);
 
-    let t_deg:(u16, f32, char) = (18988, 3.45, 'A');
+    let t_deg: (u16, f32, char) = (18988, 3.45, 'A');
     println!("{:?}", t_deg);
     println!("{}", t_deg.0);
     println!("{}", t_deg.2);
 
     //array -> boyutu sabit
-    let arr_deg = [1;10];
-    let arr_deg2:[u16; 3] = [1, 2, 3];
-    let arr_deg3:[u32; 10] = [12 ; 10];
+    let arr_deg = [1; 10];
+    let arr_deg2: [u16; 3] = [1, 2, 3];
+    let arr_deg3: [u32; 10] = [12; 10];
 
-    println!("deg2 : {}",arr_deg2[2]);
+    println!("deg2 : {}", arr_deg2[2]);
     //array_func_first(arr_deg);
 
     let ilk_eleman = array_func_first(arr_deg3);
-    println!("ilk eleman: {}",ilk_eleman);
+    println!("ilk eleman: {}", ilk_eleman);
     println!("{:?}", arr_deg3);
 
     beni_cagir();
@@ -81,16 +79,92 @@ fn main() {
 
     let don_deg = beni_cagir_don();
     println!("{}", don_deg);
-    println!("{}", beni_formatla(89))
+    println!("{}", beni_formatla(89));
 
+    //Koşullar
+    //Büyüktür -> > , Küçüktür -> <, Eşit = = -> ==
+
+    if 18 > 17
+    //true
+    {
+        println!("18 sayısı 17'den büyüktür");
+    }
+
+    if 1 == 1 {
+        println!("İki sayı birbirine eşittir");
+    }
+
+    if 1 < 0 {
+        println!("Ben hiç ekrana yazdırılmadım.");
+    } else {
+        println!("1 sayısı 0 sayısından küçük değildir. Büyük veya eşittir.");
+    }
+
+    let deg1 = if 5 > 6 {
+        "5 Sayısı 6 Sayısından büyüktür"
+    } else {
+        "5 Sayısı 6 Sayısından küçük veya eşittir."
+    };
+
+    println!("deg1 : {}", deg1);
+
+    let deg2 = if 5 > 6 {
+        "5 Sayısı 6 Sayısından büyüktür"
+    } else if 5 == 6 {
+        "5 Sayısı 6 Sayısına eşittir."
+    } else {
+        "5 Sayısı 6 Sayısından küçüktür"
+    };
+
+    let deg3: i8 = if 5 > 6 { 1 } else { 0 };
+
+    println!("deg2 : {}", deg2);
+
+    let s_deg = 1;
+    let sa_deg = 0;
+
+    if s_deg < sa_deg {
+        println!("1 sayısı 0 sayısından küçüktür");
+    } else if s_deg == sa_deg {
+        println!("1 sayısı 0 sayısına eşittir");
+    } else {
+        println!("1 sayısı 0 sayısından büyüktür.");
+    }
+
+    //Döngüler
+    // 3 tip döngü
+    // loop döngüsü -> loop -> içeride bir devrekesici, döngü kırıcı
+    //while döngüsü -> while -> koşula göre devam etme durumu
+    //for döngüsü -> for -> sınırları belli bir döngü
+
+    /*for _ in 1..=10 {
+    println!("Türkiye Rust Community");
+    }*/
+
+    let mut index = 0;
+
+    /*while index < 10 {
+    println!("Türkiye Rust Community");
+    index = index + 1;
+    }*/
+
+    //index = 0;
+
+    /*  loop {
+    println!("Türkiye Rust Community");
+    if index >= 9 {
+        break;
+    }
+    index += 1;
+    }*/
 }
 
-fn beni_cagir(){
-    println!("Ben çağrıldım.")
+fn beni_cagir() {
+    println!("Ben çağrıldım.");
 }
 
 fn beni_cagir2(param: u32) {
-    println!("Gelen Parametre: {}",param);
+    println!("Gelen Parametre: {}", param);
 }
 
 fn beni_cagir_don() -> u32 {
@@ -100,11 +174,9 @@ fn beni_cagir_don() -> u32 {
 }
 
 fn beni_formatla(param: u32) -> String {
-    format!("Dönen ve alınan değer: {}",param)
+    format!("Dönen ve alınan değer: {}", param)
 }
 
-fn array_func_first(array : [u32;10]) -> u32 {
+fn array_func_first(array: [u32; 10]) -> u32 {
     array[0]
 }
-
-
